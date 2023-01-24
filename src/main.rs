0abba6eb47f11ac6581113ee10c5657dd55e1861
Albert Lee <lee3445@purdue.edu>
2023-01-24 13:18:17 -0500

    add basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+// command line argumand parser
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    // Print modules in order of trustworthiness
+    Url { url_file: String },
+
+    // Parse results of tests
+    Report { report_file: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    // parse command line arguments
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Url { url_file: f } => println!("url: {:?}", f),
+        Commands::Report { report_file: f } => println!("test: {:?}", f),
+    }
 }
