9c979107288d063be597074b73c3f73091ed19c2
Albert Lee <lee3445@purdue.edu>
2023-01-25 23:37:41 -0500

    add basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use log::{debug, info, LevelFilter};
 
 // command line argumand parser
 #[derive(Parser)]
@@ -8,14 +9,28 @@ struct Cli {
 }
 #[derive(Subcommand)]
 enum Commands {
-    // Print modules in order of trustworthiness
+    /// Print modules in order of trustworthiness
     Url { url_file: String },
 
-    // Parse results of tests
+    /// Parse results of tests
     Report { report_file: String },
 }
 
 fn main() {
+    // setup logging level
+    let level = std::env::var("LOG_LEVEL")
+        .ok()
+        .and_then(|i| i.parse::<u8>().ok());
+    let filter = match &level {
+        Some(2) => LevelFilter::Debug,
+        Some(1) => LevelFilter::Info,
+        _ => LevelFilter::Off,
+    };
+    env_logger::Builder::new().filter_level(filter).init();
+
+    info!("print info");
+    debug!("print debug");
+
     // parse command line arguments
     let cli = Cli::parse();
 
