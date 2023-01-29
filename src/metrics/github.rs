bdd3bb3cb5dfc7db693b0f42ef8acf2e7f90c5d5
Albert Lee <lee3445@purdue.edu>
2023-01-29 12:30:44 -0500

    add tests for with_url()

--- src/metrics/github.rs
+++ src/metrics/github.rs
@@ -32,7 +32,52 @@ impl Github {
     }
 }
 impl Metrics for Github {
+    fn ramp_up_time(&self) -> f64 {
+        0.0
+    }
+
     fn correctness(&self) -> f64 {
-        1.0
+        0.0
+    }
+
+    fn bus_factor(&self) -> f64 {
+        0.0
+    }
+
+    fn responsiveness(&self) -> f64 {
+        0.0
+    }
+
+    fn compatibility(&self) -> f64 {
+        0.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn construct_with_url() {
+        let a = Github::with_url("https://github.com/lee3445/ECE461_Team19_CLI").unwrap();
+
+        assert_eq!(a.owner, "lee3445");
+        assert_eq!(a.repo, "ECE461_Team19_CLI");
+    }
+
+    #[test]
+    fn construct_with_bad_url() {
+        assert!(Github::with_url("not an url").is_none());
+    }
+    #[test]
+    fn construct_with_wrong_domain() {
+        assert!(Github::with_url(
+            "https://doc.rust-lang.org/rust-by-example/testing/unit_testing.html"
+        )
+        .is_none());
+    }
+    #[test]
+    fn construct_with_bad_github_url() {
+        assert!(Github::with_url("https://github.com/rust-lang").is_none());
     }
 }
