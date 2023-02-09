82286326d36c45a4fa7f4e74cd61ff4e8b2480b4
josephlin8909 <josephlin89@gmail.com>
2023-02-09 09:10:12 +0800

    finished Github part with one error at line 92 in main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod metrics;
 
 use clap::{Parser, Subcommand};
 use log::{debug, info, LevelFilter};
+use std::{io::{BufRead, BufReader}, collections::HashMap};
+use metrics::Metrics;
+use metrics::github::Github;
+use std::io::Write;
 
 // command line argumand parser
 #[derive(Parser)]
@@ -61,4 +65,49 @@ fn main() {
         Commands::Url { url_file: f } => println!("url: {:?}", f),
         Commands::Report { report_file: f } => println!("test: {:?}", f),
     }
+
+    let mut net_scores = Vec::new();
+
+    let file = std::fs::File::open("file.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if let Some(domain) = reqwest::Url::parse(&line).unwrap().domain() {
+            println!("{}", domain);
+            let project:Box<dyn Metrics> = Box::new(Github::with_url(&line).unwrap());
+            let mut net_score = HashMap::new();
+            let ramp_up: f64 = project.ramp_up_time();
+            let correctness: f64 = project.correctness();
+            let bus_factor: f64 = project.bus_factor();
+            let responsiveness: f64 = project.responsiveness();
+            let compatibility: f64 = project.compatibility();
+            let score: f64 = ramp_up * 0.05 + correctness * 0.1 + bus_factor * 0.1 + responsiveness * 0.25 + compatibility * 0.5;
+            net_score.insert("URL", line);
+            net_score.insert("NET_SCORE", score.to_string());
+            net_score.insert("RAMP_UP_SCORE", ramp_up.to_string());
+            net_score.insert("CORRECTNESS_SCORE", correctness.to_string());
+            net_score.insert("BUS_FACTOR_SCORE", bus_factor.to_string());
+            net_score.insert("RESPONSIVE_MAINTAINER_SCORE", responsiveness.to_string());
+            net_score.insert("LICENSE_SCORE", compatibility.to_string());
+            net_scores.push(net_score);
+
+            net_scores.sort_by(|a, b| b["NET_SCORE"].parse::<f64>().unwrap().partial_cmp(&a["NET_SCORE"].parse::<f64>().unwrap()).unwrap());
+
+            let stdout = std::io::stdout();
+            let mut handle = stdout.lock();
+
+            for dict in net_scores {
+                for (key, value) in dict {
+                    handle.write_fmt(format_args!("({}, {})\n", key, value));
+                }
+            }
+        } else if let Some(domain) = reqwest::Url::parse(&line).unwrap().domain(){
+            println!("{}", domain);
+        } else {
+            println!("neither");
+        }
+    }
+
+
 }
