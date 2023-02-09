55db0465bf1a5a5e09bcd199449e98e862cc87c5
Albert Lee <lee3445@purdue.edu>
2023-02-09 08:31:29 -0500

    random graphql stuff

--- src/req.rs
+++ src/req.rs
@@ -1,16 +1,15 @@
 use reqwest::Result;
 
 pub fn stuff() -> Result<()> {
-    let github_token = std::env::var("GITHUB_TOKEN").unwrap();
+    let s = "{\"query\": \"query { repository(owner:\\\"ppy\\\", name:\\\"osu\\\") { mentionableUsers { totalCount } } }\"}";
+    println!("{}", s);
     let client = reqwest::blocking::Client::builder()
-        .user_agent("lee3445")
+        .user_agent("ECE461_Team19_CLI")
         .build()?;
     let body = client
-        .get("https://api.github.com/repos/seanmonstar/reqwest/stargazers")
-        // .get("https://api.github.com/octocat")
-        .header(reqwest::header::AUTHORIZATION, github_token)
-        .header(reqwest::header::ACCEPT, "application/vnd.github+json")
-        .header("X-GitHub-Api-Version", "2022-11-28")
+        .post("https://api.github.com/graphql")
+        .bearer_auth(env!("GITHUB_TOKEN"))
+        .body(s)
         .send()?
         .text()?;
 
