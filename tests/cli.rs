3bf2cdfde19c642aff9544cfb9c2664e31e00ac7
Albert Lee <lee3445@purdue.edu>
2023-02-11 08:56:53 -0500

    add more tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -20,3 +20,15 @@ fn bad_arguments() {
     let mut cmd3 = Command::cargo_bin("ece461_team19_cli").unwrap();
     cmd3.arg("report").assert().failure();
 }
+
+#[test]
+fn bad_file_name() {
+    let mut cmd = Command::cargo_bin("ece461_team19_cli").unwrap();
+    cmd.args(["url", "notafile.osu"]).assert().failure();
+}
+
+#[test]
+fn empty_file() {
+    let mut cmd = Command::cargo_bin("ece461_team19_cli").unwrap();
+    cmd.args(["url", "empty.txt"]).assert().success().stdout("");
+}
